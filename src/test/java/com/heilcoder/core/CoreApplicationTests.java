fa83a81b4bf22a9c9a9412a86492fdec2411d0ee
Jason <jasonwell@139.com>
2019-07-09 00:53:23 +0800

    单元测试构建、文档整理

--- src/test/java/com/heilcoder/core/CoreApplicationTests.java
+++ src/test/java/com/heilcoder/core/CoreApplicationTests.java
@@ -5,12 +5,31 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.heilcoder.core.controller.BaseController;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CoreApplicationTests {
+public class CoreApplicationTests extends BaseController{
 
 	@Test
-	public void contextLoads() {
+	public void successTest() {
+        System.out.println("测试success()");
+        System.out.println(success("测试"));
+        System.out.println(success());
+        System.out.println(success(123456789));
+        System.out.println(success(false));
 	}
-
+	@Test
+	public void errorTest() {
+        System.out.println("测试error()");
+        System.out.println(error(0));
+        System.out.println(error(2));
+        System.out.println(error("测试error自定义message"));
+        System.out.println(error(0,"测试error传入code为0时被替换为默认值1"));
+        System.out.println(error(2,"测试error自定义message"));
+        System.out.println(error(3,"测试error传入data",false));
+        System.out.println(error(4,"测试error传入data",123456));
+        System.out.println(error(123));
+        System.out.println(error(false));
+	}
 }
